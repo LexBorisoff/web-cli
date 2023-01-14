5041f96e384982aad352f542b32a2667236b31e6
Lex Borisov <hello@lexborisov.com>
2023-01-14 17:29:40 -0500

    updated command directory types and imports

--- src/command/index.ts
+++ src/command/index.ts
@@ -1,6 +1,4 @@
-import getArgs from "./getArgs";
-import hasEngine from "./hasEngine";
-import hasSearchQuery from "./hasSearchQuery";
-import hasWebsite from "./hasWebsite";
-
-export { getArgs, hasEngine, hasSearchQuery, hasWebsite };
+export { default as getArgs } from "./getArgs";
+export { default as hasEngine } from "./hasEngine";
+export { default as hasSearchQuery } from "./hasSearchQuery";
+export { default as hasWebsite } from "./hasWebsite";
