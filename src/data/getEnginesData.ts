1877bf86c7fa43bb832fa1f8b6a63acfd5cfe3f4
Lex Borisoff <hello@LexBorisoff.com>
2023-04-16 19:47:22 -0400

    keeping a list of initial engines

--- src/data/getEnginesData.ts
+++ src/data/getEnginesData.ts
@@ -1,21 +1,9 @@
-import { hasEngines, readEnginesFile, fetchEngines } from "../helpers/config";
-import { printInfo, printError } from "../helpers/print";
+import { readEnginesFile } from "../helpers/config";
 import { EnginesConfig } from "../types/engines.types";
 
 export const engineFallback = "google";
 
 export default async function getEnginesData(): Promise<EnginesConfig> {
-  if (hasEngines()) {
-    const data = readEnginesFile();
-    return JSON.parse(data);
-  }
-
-  try {
-    printInfo("Fetching engines config...");
-    const engines = await fetchEngines();
-    return engines;
-  } catch {
-    printError("An error occurred when fetching an engines list :(");
-    return {};
-  }
+  const data = readEnginesFile();
+  return JSON.parse(data);
 }
