bbac41b150d42623f7b511a0966eee870a00e0fd
Lex Borisoff <hello@LexBorisoff.com>
2023-04-16 19:13:21 -0400

    changed writeConfigFile function name, added writeEnginesFile

--- src/helpers/config/index.ts
+++ src/helpers/config/index.ts
@@ -1,8 +1,10 @@
 export * from "./getFileName";
+export * from "./writeFile";
 export { default as getConfigItem } from "./getConfigItem";
 export {
   default as hasConfig,
   configFileExists,
   configFileIsEmpty,
 } from "./hasConfig";
-export { default as writeConfigFile } from "./writeConfigFile";
+export { default as hasEngines, readEnginesFile } from "./hasEngines";
+export { default as fetchEngines } from "./fetchEngines";
---
+++ src/helpers/config/writeFile.ts
@@ -0,0 +1,25 @@
+import * as fs from "fs";
+import { getConfigFileName, getEnginesFileName } from "./getFileName";
+import { Config } from "../../types/data.types";
+import { EnginesConfig } from "../../types/engines.types";
+
+const configFileName = getConfigFileName();
+const enginesFileName = getEnginesFileName();
+
+export function writeConfigFile(config: Config): void {
+  const json = JSON.stringify(config);
+  fs.writeFile(configFileName, json, (error) => {
+    if (error != null) {
+      throw error;
+    }
+  });
+}
+
+export function writeEnginesFile(engines: EnginesConfig): void {
+  const json = JSON.stringify(engines);
+  fs.writeFile(enginesFileName, json, (error) => {
+    if (error != null) {
+      throw error;
+    }
+  });
+}
