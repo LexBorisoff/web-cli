31fc6b4ada1093fc63904e9deb86bdaf4775550a
Lex Borisoff <hello@LexBorisoff.com>
2023-07-11 20:04:01 -0400

    prompting for engine name and base url

--- src/config/change/add/engine.ts
+++ src/config/change/add/engine.ts
@@ -1,4 +1,84 @@
+import chalk from "chalk";
+import { getEnginesData } from "../../../data";
+import { cliPrompts } from "../../../helpers/prompts";
+import { enginePattern, baseUrlPattern } from "../../../helpers/patterns";
+import { emptyLine } from "../../../helpers/print";
+import { TextAnswer } from "../../../types/config.types";
+
+const { text } = cliPrompts;
+const answer: TextAnswer = {};
+
+function validateEngineName(value: string): true | string {
+  const providedName = value.trim();
+  if (!enginePattern.test(providedName)) {
+    return "Invalid engine name";
+  }
+
+  const engines = getEnginesData();
+  if (Object.keys(engines).length > 0) {
+    const engineNames = Object.values(engines).map((engine) =>
+      engine.name.toLowerCase()
+    );
+
+    if (engineNames.includes(providedName.toLowerCase())) {
+      const found = Object.values(engines).find(
+        (engine) => engine.name.toLowerCase() === providedName.toLowerCase()
+      );
+
+      let engineName = value;
+      if (found) {
+        engineName = found.name;
+      }
+
+      return `${engineName} already exists`;
+    }
+  }
+
+  return true;
+}
+
+function validateBaseUrl(value: string): true | string {
+  const providedUrl = value.trim();
+  return baseUrlPattern.test(providedUrl) || "Invalid base URL";
+}
+
+function removeWhiteSpace(value: string) {
+  let result = "";
+  for (let i = 0; i < value.length; i++) {
+    if (value[i] !== " " && value[i] !== "\t") {
+      result = `${result}${value[i]}`;
+    }
+  }
+  return result;
+}
+
 export default async function addEngine(): Promise<boolean> {
-  console.log("add engine");
+  answer.engineName = await text(
+    `Provide the ${chalk.yellowBright("engine's name")}:\n`,
+    (value) => validateEngineName(value)
+  );
+
+  if (answer.engineName == null) {
+    return false;
+  }
+
+  const engineName = answer.engineName.trim();
+
+  emptyLine();
+  answer.baseUrl = await text(
+    `Type ${engineName}'${
+      !engineName.toLowerCase().endsWith("s") && "s"
+    } ${chalk.yellowBright("base URL")} ${chalk.italic.cyanBright(
+      `(e.g. "${removeWhiteSpace(engineName.toLowerCase())}.com")`
+    )}\n`,
+    validateBaseUrl
+  );
+
+  if (answer.baseUrl == null) {
+    return false;
+  }
+
+  const baseUrl = answer.baseUrl.trim();
+
   return true;
 }
