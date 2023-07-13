3c8c0a6d43e6e944d65642788f0736e5660d1b92
Lex Borisoff <hello@LexBorisoff.com>
2023-07-13 16:17:20 -0400

    updated base url pattern

--- src/helpers/patterns.ts
+++ src/helpers/patterns.ts
@@ -2,4 +2,6 @@ export const namePattern = /^[A-Za-z]+$/;
 export const lettersNumbersPattern = /^[A-Za-z0-9]+$/;
 export const directoryPattern = /^[A-Za-z0-9 ]+$/;
 export const enginePattern = /^[A-Za-z0-9 ]+$/;
-export const baseUrlPattern = /^[a-z\d-]+\.[a-z]{2,}\/?$/is;
+
+// base url like google.com or ca.google.com w/ or w/o forward slash
+export const baseUrlPattern = /^([a-z0-9]+\.)+[a-z]{2,}\/?$/is;
