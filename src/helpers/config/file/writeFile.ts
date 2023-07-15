2c3d7c81d2c2a871705d4b27584dd8b18152de32
Lex Borisoff <hello@LexBorisoff.com>
2023-07-15 15:37:02 -0400

    accepting a string as Data in the writeFile function

--- src/helpers/config/file/writeFile.ts
+++ src/helpers/config/file/writeFile.ts
@@ -4,13 +4,13 @@ import { ConfigFileType } from "../../../types/config.types";
 import { Config } from "../../../types/data.types";
 import { EnginesConfig } from "../../../types/engine.types";
 
-type Data = Config | EnginesConfig;
+type Data = Config | EnginesConfig | string;
 
 export default function writeFile(fileType: ConfigFileType, data: Data): void {
   const fileName = getFileName(fileType);
 
   if (fileName != null) {
-    const json = JSON.stringify(data);
+    const json = typeof data === "string" ? data : JSON.stringify(data);
     fs.writeFileSync(fileName, json);
   }
 }
