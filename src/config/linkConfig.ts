6e091aae87413162260bf3de322975f7cab589cb
Lex Borisoff <hello@LexBorisoff.com>
2023-07-26 19:06:05 -0400

    created a separate method from linkConfig to check if config file is valid

--- src/config/linkConfig.ts
+++ src/config/linkConfig.ts
@@ -15,37 +15,74 @@ const settingsPath = getSettingsPath();
 const settings = getSettings() ?? {};
 const { link: configLink } = settings;
 
-export default async function linkFile(): Promise<void> {
-  const [, ...values] = args;
+interface ValidResponse {
+  configPath: string;
+}
 
+interface InvalidResponse {
+  message: string;
+  printFormat: boolean;
+}
+
+function isValidResponse(
+  response: ValidResponse | InvalidResponse
+): response is ValidResponse {
+  return "configPath" in response;
+}
+
+/**
+ * Returns the config path if passes file validation
+ */
+function isValidConfigFile(
+  values: Partial<typeof args>
+): ValidResponse | InvalidResponse {
   if (values.length > 1) {
-    printError("Invalid number of arguments");
-    printFormat.link();
-    emptyLine();
-    return;
+    return {
+      message: "Invalid number of arguments",
+      printFormat: true,
+    };
   }
 
-  const [, configArg] = <Partial<typeof args>>args;
+  const [configArg] = values;
   if (configArg == null) {
-    printError("Filename must be provided");
-    printFormat.link();
-    emptyLine();
-    return;
+    return {
+      message: "Filename must be provided",
+      printFormat: true,
+    };
   }
 
   if (typeof configArg !== "string" || !configArg.endsWith(".json")) {
-    printError("Config must a .json file");
-    emptyLine();
-    return;
+    return {
+      message: "Config must a .json file",
+      printFormat: false,
+    };
   }
 
   const configPath = path.resolve(configArg);
   if (!fs.existsSync(configPath)) {
-    printError("Could not access the provided config file");
+    return {
+      message: "Could not access the provided config file",
+      printFormat: false,
+    };
+  }
+
+  return { configPath };
+}
+
+export default async function linkFile(): Promise<void> {
+  const [, ...values] = <Partial<typeof args>>args;
+  const validation = isValidConfigFile(values);
+
+  if (!isValidResponse(validation)) {
+    printError(validation.message);
+    if (validation.printFormat) {
+      printFormat.link();
+    }
     emptyLine();
     return;
   }
 
+  const { configPath } = validation;
   const hasConfigLink = configLink != null && configLink !== "";
 
   let proceed: boolean | undefined =
