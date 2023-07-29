7fbf85e95e6a64ad6d0b98cce77ac2c7c28f4946
Lex Borisoff <hello@LexBorisoff.com>
2023-07-28 20:32:05 -0400

    validating profile options based on the provided browser

--- src/query/profile.ts
+++ src/query/profile.ts
@@ -1,7 +1,21 @@
 import openBrowser from "./openBrowser";
 import { getProfileArgs } from "../command";
-import { getDefaultsData } from "../data";
+import { getDefaultsData, getProfilesData } from "../data";
 import { getProfile } from "../helpers/browser";
+import { printWarning, printError, emptyLine } from "../helpers/print";
+import { getTitle } from "../helpers/prompts";
+
+function getProfileFlags(browserName: string) {
+  const profilesData = getProfilesData(browserName);
+  return Object.entries(profilesData)
+    .map(([key, { alias }]) => {
+      if (alias != null) {
+        return Array.isArray(alias) ? [key, ...alias] : [key, alias];
+      }
+      return key;
+    })
+    .flat();
+}
 
 export default function queryProfile(browserName: string, url?: string): void {
   function handleProfile(profileDirectory?: string): void {
@@ -9,11 +23,33 @@ export default function queryProfile(browserName: string, url?: string): void {
   }
 
   const profileArgs = getProfileArgs(browserName);
+  const profileFlags = getProfileFlags(browserName);
+
+  // single option was provided without a value
+  const isEmptyArg = profileArgs.length === 1 && profileArgs[0] === "";
+  if (isEmptyArg) {
+    printError("Profile option must have a value");
+    emptyLine();
+    return;
+  }
+
+  // invalid profile values were supplied (excluding empty value)
+  const invalidProfiles = profileArgs.filter(
+    (arg) => arg !== "" && !profileFlags.includes(arg)
+  );
+  if (invalidProfiles.length > 0) {
+    printError(`Invalid ${getTitle(browserName)} profiles:`);
+    printWarning(invalidProfiles.join(", "));
+    emptyLine();
+    return;
+  }
+
+  const validProfiles = profileArgs.filter((arg) => arg !== "");
   const defaultProfile = getDefaultsData().profile?.[browserName];
 
   // profile(s) provided in args
-  if (profileArgs.length > 0) {
-    profileArgs.forEach((profileNameOrAlias) => {
+  if (validProfiles.length > 0) {
+    validProfiles.forEach((profileNameOrAlias) => {
       const profile = getProfile(profileNameOrAlias, browserName);
       handleProfile(profile?.directory);
     });
