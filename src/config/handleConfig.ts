7a56f7bb025d9ce9b653d661cc805cf0dd0d9c52
Lex Borisoff <hello@LexBorisoff.com>
2023-08-19 17:07:58 -0400

    refactored handleConfig which now handles creating a config folder in OS' AppData

--- src/config/handleConfig.ts
+++ src/config/handleConfig.ts
@@ -1,117 +1,35 @@
-import chalk from "chalk";
-import { getConfigArgs } from "../command/args";
-import {
-  printInfo,
-  printError,
-  printWarning,
-  emptyLine,
-} from "../helpers/print";
-import initConfig from "./initConfig";
-import openConfig from "./openConfig";
-import validateConfig from "./validateConfig";
-import linkConfig from "./linkConfig";
-import unlinkConfig from "./unlinkConfig";
-import whereConfig from "./whereConfig";
-import cacheConfig from "./cacheConfig";
-import usingConfig from "./usingConfig";
-import listConfig from "./listConfig";
-import helpConfig from "./helpConfig";
+import * as fs from "fs";
+import { getConfigPath } from "../helpers/config";
+import { severity } from "../helpers/print";
 
-/**
- * < > required argument
- * [ ] optional argument
- *
- * --config init [directory]                  -> initializes a config file
- *                                            (in current directory if not provided)
- * --config open [app]                        -> opens config (in deafult app if not provided)
- * --config validate                          -> checks if config passes validation
- * --config link <filename>                   -> links a config file
- * --config unlink                            -> unlinks the config file
- * --config where                             -> shows the path to the linked config file
- * --config cache [clear]                     -> caches data of the linked config file
- *                                            ("clear" argument deletes the cached data)
- * --config cache <filename>                  -> caches data of the provided config file
- * --config cache export [filename]           -> exports cache into the cachedPath or provided file
- * --config using                             -> shows if cached, linked, or no config is used
- * --config list <defaults|browsers|engines>  -> lists config contents
- * --config help                              -> help with config commands
- */
+const configPath = getConfigPath();
 
-const { _: args } = getConfigArgs();
-
-enum ConfigOption {
-  init = "init",
-  open = "open",
-  validate = "validate",
-  link = "link",
-  unlink = "unlink",
-  where = "where",
-  cache = "cache",
-  using = "using",
-  list = "list",
-  help = "help",
+function createConfigDirectory(): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(configPath, { recursive: true }, (error) => {
+      if (error != null) {
+        reject(new Error("Could not create config folder."));
+      } else {
+        resolve(true);
+      }
+    });
+  });
 }
 
-const validOptions = Object.values(ConfigOption) as string[];
+export default async function handleConfig() {
+  let configExists = fs.existsSync(configPath);
 
-function isValidOption(
-  option: ReturnType<typeof args.at>
-): option is ConfigOption {
-  return typeof option === "string" && validOptions.includes(option);
-}
-
-export default async function handleConfig(): Promise<void> {
-  const [option] = <Partial<typeof args>>args;
-
-  if (option == null) {
-    printWarning("--config must be used with a valid command:");
-    printInfo(
-      [...validOptions]
-        .sort((a, b) => a.localeCompare(b))
-        .join(chalk.gray(" | "))
-    );
-    emptyLine();
-    return;
+  if (!configExists) {
+    try {
+      configExists = await createConfigDirectory();
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(severity.error(error.message));
+      }
+    }
   }
 
-  if (!isValidOption(option)) {
-    printError("Invalid config command");
-    emptyLine();
-    return;
+  if (configExists) {
+    //
   }
-
-  switch (option) {
-    case ConfigOption.init:
-      await initConfig();
-      break;
-    case ConfigOption.open:
-      openConfig();
-      break;
-    case ConfigOption.validate:
-      validateConfig();
-      break;
-    case ConfigOption.link:
-      linkConfig();
-      break;
-    case ConfigOption.unlink:
-      unlinkConfig();
-      break;
-    case ConfigOption.where:
-      whereConfig();
-      break;
-    case ConfigOption.cache:
-      await cacheConfig();
-      break;
-    case ConfigOption.using:
-      usingConfig();
-      break;
-    case ConfigOption.list:
-      listConfig();
-      break;
-    case ConfigOption.help:
-      helpConfig();
-      break;
-  }
-
-  emptyLine();
 }
