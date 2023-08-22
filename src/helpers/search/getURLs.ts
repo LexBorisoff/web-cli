a657a4fa541f77dd24f9736060859294bf354ccb
Lex Borisoff <hello@LexBorisoff.com>
2023-08-21 20:49:48 -0400

    refactored the --route option in getURLs

--- src/helpers/search/getURLs.ts
+++ src/helpers/search/getURLs.ts
@@ -2,10 +2,12 @@ import chalk from "chalk";
 import getEngine from "./getEngine";
 import { urlPattern } from "../patterns";
 import { emptyLine, printWarning, printError } from "../print";
-import { withEngine, withSearchQuery, withURL } from "../../command";
+import { withEngine, withRoute, withValues, withURL } from "../../command/with";
 import { getQueryArgs, getDataArgs } from "../../command/args";
+import { orArray } from "../../command/args/utils";
 import { getDefaultsData } from "../../data";
 import { Engine } from "../../types/config.types";
+import { Arg } from "../../types/utility.types";
 
 const args = getQueryArgs();
 const urlArgs = args._.map((arg) =>
@@ -18,12 +20,12 @@ const noQueryEngines: string[] = [];
 const trailingSlash = /\/$/;
 const leadingSlash = /^\//;
 
-function removeLeadingSlash(str?: string): string {
-  if (str == null) {
-    return "";
-  }
+function removeLeadingSlash(value: string): string {
+  return leadingSlash.test(value) ? value.substring(1) : value;
+}
 
-  return leadingSlash.test(str) ? str.substring(1) : str;
+function addTrailingSlash(value: string): string {
+  return trailingSlash.test(value) ? value : `${value}/`;
 }
 
 /**
@@ -31,7 +33,7 @@ function removeLeadingSlash(str?: string): string {
  * with a trailing forward slash
  */
 function getEngineBaseURL(engine: Engine): string {
-  return trailingSlash.test(engine.url) ? engine.url : `${engine.url}/`;
+  return addTrailingSlash(engine.url);
 }
 
 /**
@@ -40,11 +42,6 @@ function getEngineBaseURL(engine: Engine): string {
  */
 function getEngineQueryURL(engine: Engine, searchQuery: string): string {
   const baseURL = getEngineBaseURL(engine);
-
-  if (args.package && engine.package != null) {
-    const engineQuery = removeLeadingSlash(engine.package);
-    return baseURL + engineQuery + searchQuery;
-  }
 
   if (engine.query != null) {
     const engineQuery = removeLeadingSlash(engine.query);
@@ -55,9 +52,17 @@ function getEngineQueryURL(engine: Engine, searchQuery: string): string {
   return baseURL;
 }
 
-function getEngineRoute(engine: Engine, route: string): string {
+/**
+ * Returns the provided engine's route URL
+ *
+ * @param routeArg
+ * if it refers to a route in the engine's config, then the URL is built
+ * using the config's route value, otherwise the argument itself is used
+ * to create the URL
+ */
+function getEngineRouteURL(engine: Engine, routeArg: string): string {
   const found = Object.entries(engine.routes ?? {}).find(
-    ([key]) => key === route
+    ([key]) => key === routeArg
   );
 
   if (found != null) {
@@ -65,7 +70,7 @@ function getEngineRoute(engine: Engine, route: string): string {
     return getEngineBaseURL(engine) + route;
   }
 
-  return getEngineBaseURL(engine) + route;
+  return getEngineBaseURL(engine) + routeArg;
 }
 
 /**
@@ -74,7 +79,7 @@ function getEngineRoute(engine: Engine, route: string): string {
  * @param engineNameOrAlias
  * if not provided, the default engine will be used
  */
-function constructURLs(engineNameOrAlias?: string): string[] {
+function createURLs(engineNameOrAlias?: string): string[] {
   const urls: string[] = [];
   const [, engine] = getEngine(engineNameOrAlias ?? defaults.engine) ?? [];
 
@@ -83,16 +88,11 @@ function constructURLs(engineNameOrAlias?: string): string[] {
     return /^https?:\/\//is.test(url) ? url : `${protocol}${url}`;
   }
 
-  function singleSearchQuery(): void {
+  /**
+   * Constructs simple search query URLs
+   */
+  function searchQuery(): void {
     if (engine == null) {
-      return;
-    }
-
-    if (args.route) {
-      args._.forEach((route) => {
-        const routeURL = getEngineRoute(engine, route.toString());
-        urls.push(getFullURL(routeURL));
-      });
       return;
     }
 
@@ -110,15 +110,62 @@ function constructURLs(engineNameOrAlias?: string): string[] {
     urls.push(getFullURL(engineQuery));
   }
 
+  /**
+   * Constructs engine route URLs
+   *
+   * @param arg
+   * if provided, it is added to the URL after a forward-slash
+   */
+  function engineRouteQuery(arg?: Arg) {
+    function createRoute(route: string) {
+      if (engine == null) {
+        return;
+      }
+
+      const engineRoute = getEngineRouteURL(engine, route);
+      const routeURL =
+        arg != null
+          ? addTrailingSlash(engineRoute) + arg.toString()
+          : engineRoute;
+
+      urls.push(getFullURL(routeURL));
+    }
+
+    const route = orArray(args.route);
+
+    if (Array.isArray(route)) {
+      route
+        .filter((r) => r !== "")
+        .forEach((r) => {
+          createRoute(r);
+        });
+    } else if (route != null) {
+      createRoute(route);
+    }
+  }
+
+  // routes
+  if (withRoute) {
+    // query engine routes
+    if (args._.length > 0) {
+      args._.forEach((arg) => {
+        engineRouteQuery(arg);
+      });
+    }
+    // access engine routes
+    else {
+      engineRouteQuery();
+    }
+  }
   // search query
-  if (withSearchQuery) {
-    singleSearchQuery();
+  else if (withValues) {
+    searchQuery();
   }
   // URL
   else if (withURL) {
     // single search query with URL args as part of the query string
     if (args.query) {
-      singleSearchQuery();
+      searchQuery();
     }
     // full URLs based on the provided URL args
     else if (engineNameOrAlias == null) {
@@ -138,7 +185,7 @@ function constructURLs(engineNameOrAlias?: string): string[] {
     }
   }
   // engine only
-  else if (engineNameOrAlias != null && args.route == null) {
+  else if (engineNameOrAlias != null) {
     const [, engine] = getEngine(engineNameOrAlias) ?? [];
     if (engine != null) {
       urls.push(getFullURL(engine.url));
@@ -155,13 +202,13 @@ export default function getURLs(): string[] {
   const urls: string[] = [];
   const engineArgs = getDataArgs.engine();
 
-  if (withEngine || withSearchQuery || withURL) {
+  if (withEngine || withValues || withURL) {
     if (engineArgs.length > 0) {
       engineArgs.forEach((engineNameOrAlias) => {
-        urls.push(...constructURLs(engineNameOrAlias));
+        urls.push(...createURLs(engineNameOrAlias));
       });
     } else {
-      urls.push(...constructURLs());
+      urls.push(...createURLs());
     }
   }
 
