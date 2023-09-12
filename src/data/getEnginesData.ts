efc990a31d4fa14465a0b98e5527bb800f8dcc23
Lex Borisoff <hello@lexborisoff.com>
2023-09-12 19:47:50 -0400

    using config engines only if there is config data

--- src/data/getEnginesData.ts
+++ src/data/getEnginesData.ts
@@ -5,5 +5,5 @@ import { initialEngines } from "../helpers/config";
 
 export default function getEnginesData(): EnginesData {
   const configData = getConfigData(ConfigValue.Engines);
-  return Object.assign(initialEngines, configData);
+  return Object.keys(configData).length > 0 ? configData : initialEngines;
 }
