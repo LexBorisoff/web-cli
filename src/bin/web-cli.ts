1e9e09cd875bc3d0733d65d316ff2cd10f2075c8
Lex Borisoff <hello@lexjs.com>
2023-09-29 21:13:24 -0400

    renamed main in bin to web-cli

--- src/bin/web-cli.ts
+++ src/bin/web-cli.ts
@@ -4,4 +4,8 @@ import query from "./query.js";
 import { withConfig } from "./command/with.js";
 import handleConfig from "./config/handleConfig.js";
 
-withConfig ? handleConfig() : query();
+if (withConfig) {
+  handleConfig();
+} else {
+  query();
+}
