4a5ec3238f19aeb02d344a4aa61df6919dd738c2
Lex Borisoff <hello@lexjs.com>
2023-09-30 23:32:21 -0400

    fixed getVersion in esm

--- src/bin/helpers/getVersion.ts
+++ src/bin/helpers/getVersion.ts
@@ -1,16 +1,22 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
+import * as url from "node:url";
 
 export default function getVersion(): string {
   let version = "Could not fetch version";
 
+  /**
+   * root
+   *  ./dist/bin/helpers/getVersion.js
+   *  ./package.json
+   */
   try {
-    const json = fs.readFileSync(
-      path.resolve(`${__dirname}/../../package.json`),
-      {
-        encoding: "utf-8",
-      }
-    );
+    const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+    const root = path.join(__dirname, "../../../");
+
+    const json = fs.readFileSync(path.resolve(`${root}/package.json`), {
+      encoding: "utf-8",
+    });
 
     const data = JSON.parse(json) as { version?: string };
 
