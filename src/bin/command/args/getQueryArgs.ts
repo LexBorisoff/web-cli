4dd6ecb866fae1d2eda1bd1261c188c7ad946aef
Lex Borisoff <hello@lexjs.com>
2023-10-01 03:19:17 -0400

    added option descriptions and enabled the help option

--- src/bin/command/args/getQueryArgs.ts
+++ src/bin/command/args/getQueryArgs.ts
@@ -9,38 +9,47 @@ export default function getQueryArgs() {
     .option(Option.Browser, {
       type: "string",
       alias: alias.browser,
+      description: "A browser app to open",
     })
     .option(Option.Profile, {
       type: "string",
       alias: alias.profile,
+      description: "A browser profile to use",
     })
     .option(Option.Engine, {
       type: "string",
       alias: alias.engine,
+      description: "A search engine (or website) to query",
     })
     .option(Option.Route, {
       type: "string",
       alias: alias.route,
+      description: "An engine's route to access",
     })
     .option(Option.Address, {
       type: "string",
       alias: alias.address,
+      description: "A custom address to access",
     })
     .option(Option.Port, {
       type: "number",
       alias: alias.port,
+      description: "An explicit port number to add to the URL",
     })
     .option(Option.Incognito, {
       type: "boolean",
       alias: alias.incognito,
+      description: "Open in incognito / private mode",
     })
     .option(Option.Split, {
       type: "boolean",
+      description: "Split values into separate web queries",
     })
     .option(Option.Http, {
       type: "boolean",
+      description: "Use the HTTP (non-secure) protocol",
     })
-    .help(false)
+    .help()
     .version(getVersion())
     .boolean(configFlags.filter((flag) => !options.includes(flag)))
     .parseSync();
