b2aeb4d599497d89e698a8e6df2f4650052a76c3
Lex Borisoff <hello@lexjs.com>
2023-10-04 18:01:36 -0400

    deleted unused browser hepers

--- src/bin/helpers/browser/index.ts
+++ src/bin/helpers/browser/index.ts
@@ -1,4 +1,2 @@
-export { default as getBrowserAppName } from "./getBrowserAppName.js";
-export { default as getBrowserArguments } from "./getBrowserArguments.js";
 export { default as getBrowserName } from "./getBrowserName.js";
 export { default as getProfiles } from "./getProfiles.js";
