0394008582df39ca08f6955c379ee27bdb5c6ca1
Lex Borisoff <helo@LexBorisoff.com>
2024-04-03 19:03:33 -0400

    fixed url pattern to allow hyphenated urls

--- src/bin/utilities/patterns.ts
+++ src/bin/utilities/patterns.ts
@@ -1,9 +1,5 @@
 /**
- * prohibits path or query params
- */
-export const baseUrlPattern = /^([A-Za-z0-9]+\.)+[a-z]{2,}\/?$/is;
-
-/**
  * allows path and query params
  */
-export const urlPattern = /^(https?:\/\/)?([A-Za-z0-9]+\.)+[a-z]{2,}\/?/is;
+export const urlPattern =
+  /^(https?:\/\/)?(\b([A-Za-z0-9]+([A-Za-z0-9-])*([A-Za-z0-9])*\b)\.)+[a-z]{2,}\/?/is;
