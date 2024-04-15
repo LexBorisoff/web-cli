06523be4ef1f17d082b6d44cb033687868d009f8
Lex Borisoff <lex@borisoff.dev>
2024-04-15 18:32:31 -0400

    renamed get-version file

--- src/bin/helpers/get-version.ts
+++ src/bin/helpers/get-version.ts
@@ -1,23 +1,19 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
-import * as url from "node:url";
 
-export default function getVersion(): string {
+export function getVersion(): string {
   let version = "Could not fetch version";
 
   /**
    * root
-   *  ./dist/bin/helpers/getVersion.js
+   *  ./bin/helpers/get-version.js
    *  ./package.json
    */
   try {
-    const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
-    const root = path.join(__dirname, "../../../");
-
+    const root = path.join(__dirname, "../../");
     const json = fs.readFileSync(path.resolve(`${root}/package.json`), {
       encoding: "utf-8",
     });
-
     const data = JSON.parse(json) as { version?: string };
 
     if (data.version != null) {
