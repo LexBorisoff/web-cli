69f520ae1d3e0e11ef3969644e0a296774a671f1
Lex Borisoff <lex@borisoff.dev>
2024-04-15 18:08:51 -0400

    renamed index and update-version files

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import query from "./query.js";
-import updateVersion from "./updateVersion.js";
+import updateVersion from "./update-version.js";
 import { withUpdate, withConfig } from "./command/with.js";
 import handleConfig from "./config/handleConfig.js";
 
