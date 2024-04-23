27ce9c9440ec47661f6b6db54d5fededbd5342f3
Lex Borisoff <lex@borisoff.dev>
2024-04-23 13:04:53 -0400

    deleted old query file

--- src/query/query.ts
+++ src/query/query.ts
@@ -1,13 +1,9 @@
 import { validateArgs } from "../command/args/validate-args.js";
 import { print } from "../helpers/print/severity.js";
-import { getQueryArgs } from "../command/args/get-query-args.js";
-import { BrowserQuery } from "../types/browser-query.type.js";
 import { getEngines } from "./get-engines.js";
 import { openUrls } from "./open-urls.js";
 import { getUrls } from "./get-urls.js";
 import { printQuery } from "./print-query.js";
-
-const options = getQueryArgs();
 
 export async function query(): Promise<void> {
   const errors = validateArgs();
