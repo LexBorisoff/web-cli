21dc0b77be63dd3bf550388e4640c79ec4979912
Lex Borisoff <lex@borisoff.dev>
2024-04-23 22:18:47 -0400

    handling create new config project and show config project dir

--- src/config/handle-config.ts
+++ src/config/handle-config.ts
@@ -1,1 +1,21 @@
-export function handleConfig() {}
+import { getConfigAction, ConfigAction } from "./get-config-action.js";
+import { createConfigProject } from "./create-config-project/create-config-project.js";
+import { showConfigProjectDir } from "./show-project-dir/show-project-dir.js";
+
+export async function handleConfig() {
+  const action = await getConfigAction();
+
+  if (action == null) {
+    return;
+  }
+
+  if (action === ConfigAction.CreateProject) {
+    await createConfigProject();
+    return;
+  }
+
+  if (action === ConfigAction.ShowProjectDir) {
+    showConfigProjectDir();
+    return;
+  }
+}
